7630db3ca683f038faf0be5a3b63cad26e9ebe13
Uthaeus <romanlavery@gmail.com>
2018-05-29 23:09:54 -0600

    factorials in javascript

---
+++ delta_bits.js
@@ -0,0 +1,14 @@
+// Complete the function to determine the number of bits required to convert integer A to integer B (where A and B >= 0)
+
+// There are no upper limits for A and B.
+
+// For example, you can change 31 to 14 by flipping the 4th and 0th bit:
+
+
+function convertBits(a, b){
+  
+}
+
+
+
+convertBits(31,14); //,2);
