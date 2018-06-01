3dfff26f488d40b785acc5538485af38c24ae131
Uthaeus <romanlavery@gmail.com>
2018-05-31 21:52:48 -0600

    Complete the function which converts hex number (given as a string) to a decimal number

---
+++ hex_to_dec.js
@@ -0,0 +1,12 @@
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+
+function hexToDec(hexString){
+  //your code here
+  return parseInt(hexString, 16);
+}
+
+
+
+console.log(hexToDec("a")) //, 10))
+console.log(hexToDec("FF")) //, 255);)
