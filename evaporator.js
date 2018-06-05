111f005c2f57c3c901e505fa4d0cdd31b6cf1505
Uthaeus <romanlavery@gmail.com>
2018-06-05 09:41:18 -0600

    The program reports the nth day (as an integer) on which the evaporator will be out of use.

---
+++ evaporator.js
@@ -0,0 +1,24 @@
+// This program tests the life of an evaporator containing a gas.
+
+// We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.
+
+// The program reports the nth day (as an integer) on which the evaporator will be out of use.
+
+
+function evaporator(content, evap_per_day, threshold){ 
+  actual_thresh = content * (threshold / 100);
+  count = 0;
+  currentAmt = content;
+
+  while (currentAmt >= actual_thresh) {
+    currentAmt -= currentAmt * (evap_per_day / 100);
+    count++
+  }
+
+  return count;
+}
+
+
+
+
+console.log(evaporator(10,10,10)) //,22))
