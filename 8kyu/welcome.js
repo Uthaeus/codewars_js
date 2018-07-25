3100ca6f7b2afc3acf0655a72e360ec5517c2ed0
Uthaeus <romanlavery@gmail.com>
2018-07-25 15:57:54 -0600

    Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input

---
+++ 8kyu/welcome.js
@@ -0,0 +1,32 @@
+// The Task
+// Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+// Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+
+
+function greet(language) {
+  let db = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }
+  return language.charCodeAt(language[0]) < 97 ? 'Welcome' : db[language];
+}
+
+
+console.log(greet('english')) //, 'Welcome')
+console.log(greet('dutch')) //, 'Welkom')
+console.log(greet('IP_ADDRESS_INVALID')) //, 'Welcome')
