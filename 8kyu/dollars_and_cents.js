772214f401e730a5da24d08b38202b688d3d9dd3
Uthaeus <romanlavery@gmail.com>
2018-07-26 21:32:21 -0600

    create a function that will take a float and return the amount formatting in dollars and cents

---
+++ 8kyu/dollars_and_cents.js
@@ -0,0 +1,11 @@
+// The company you work for has just been awarded a contract to build a payment gateway. In order to help move things along, you have volunteered to create a function that will take a float and return the amount formatting in dollars and cents.
+
+
+function formatMoney(amount){
+  // your formatting code here
+  return `$${amount.toFixed(2)}`
+}
+
+
+
+console.log(formatMoney(39.99)) //, '$39.99')
