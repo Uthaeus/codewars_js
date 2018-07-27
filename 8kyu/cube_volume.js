1e378c872f457a29d8e10af5c481d7badedc351e
Uthaeus <romanlavery@gmail.com>
2018-07-27 11:10:17 -0600

    Write a function to help Bob with this calculation.

---
+++ 8kyu/cube_volume.js
@@ -0,0 +1,20 @@
+// Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+
+
+var Kata;
+
+Kata = (function() {
+  function Kata() {}
+
+  Kata.getVolumeOfCuboid = function(length, width, height) {
+    // your code here
+    return length * width * height;
+  };
+
+  return Kata;
+
+})();
+
+
+
+console.log(Kata.getVolumeOfCuboid(6.3,2,5)) //, 63)
