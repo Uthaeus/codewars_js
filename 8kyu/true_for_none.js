6f02f55ebe177a8e6f3c9c28b7970356182cfaa2
Uthaeus <romanlavery@gmail.com>
2018-07-29 17:53:57 -0600

    Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array

---
+++ 8kyu/true_for_none.js
@@ -0,0 +1,14 @@
+// Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array. An empty list should return true.
+
+
+function none(arr, fun){
+  // ...
+  let result = arr.filter(fun);
+
+  return result.length == 0;
+}
+
+
+
+console.log(none([1,2,3,4,5],function(item){ return item > 4 })) 
+//, false)
