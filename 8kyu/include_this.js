2b68c869e7ea399f825e56a583327f2bf7d31abd
Uthaeus <romanlavery@gmail.com>
2018-07-29 17:56:37 -0600

    Create a method include? (JS include)that accepts a list and an item, and returns true if the item belongs to the list.

---
+++ 8kyu/include_this.js
@@ -0,0 +1,11 @@
+// Create a method include? (JS include)that accepts a list and an item, and returns true if the item belongs to the list.
+
+
+function include(arr, item){
+  // ...
+  return arr.includes(item);
+}
+
+
+
+console.log(include([1,2,4,5], 3)) //, false)
