ebb8095bb38f9029e0fa8063b1fcfe6ab6884bbc
Uthaeus <romanlavery@gmail.com>
2018-07-28 21:39:33 -0600

    return the result of multiplying the values together in order

---
+++ 8kyu/reduce_but_grow.js
@@ -0,0 +1,10 @@
+// Given and array of integers (x), return the result of multiplying the values together in order. 
+
+
+function grow(x){
+  return x.reduce((a, b) => a *= b);
+}
+
+
+
+console.log(grow([2, 2, 2, 2, 2, 2])) //, 64)
