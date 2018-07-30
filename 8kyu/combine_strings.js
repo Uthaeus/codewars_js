6166ca37889f8d5f34e9c73120b9fe242f48c3f2
Uthaeus <romanlavery@gmail.com>
2018-07-29 18:23:21 -0600

    Create a function named combineNames(combine_names in python, ruby) that accepts two parameters (first and last name)

---
+++ 8kyu/combine_strings.js
@@ -0,0 +1,10 @@
+// Create a function named combineNames(combine_names in python, ruby) that accepts two parameters (first and last name). The function should return the full name.
+
+
+function combineNames(f, l) {
+  return `${f} ${l}`;
+}
+
+
+
+console.log(combineNames('James', 'Stevens')) //, 'James Stevens')
