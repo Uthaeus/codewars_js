251dcdf777c1eb5e9986d290364effd8d7d7b0fa
Uthaeus <romanlavery@gmail.com>
2018-08-04 23:18:06 -0600

    The function date_nb_days (or dateNbDays...) with parameters a0, a, p will return, as a string, the date on which you have just reached a

---
+++ 7kyu/target_date.js
@@ -0,0 +1,10 @@
+//#Task: The function date_nb_days (or dateNbDays...) with parameters a0, a, p will return, as a string, the date on which you have just reached a.
+
+
+function dateNbDays(a0, a, p) {
+  // your code
+}
+
+
+
+console.log(dateNbDays(4620, 5188, 2)) //, "2021-09-19"
