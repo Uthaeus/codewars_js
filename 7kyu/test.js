88d2e91470d5df2c374a2ebf4ab367b1df9a13dc
Uthaeus <romanlavery@gmail.com>
2018-08-06 14:21:37 -0600

    Object destructuring

--- 7kyu/test.js
+++ 7kyu/test.js
@@ -1,18 +1,13 @@
-function whatIsInAName(collection, source) {
-  // What's in a name?
-  var arr = [];
-  // Only change code below this line
-  let myKey = Object.keys(source);
+function getLength(str) {
+  "use strict";
 
-  for (var i = 0; i < collection.length; i++) {
-    if (collection[i][myKey] == source[myKey]) {
-      arr.push(collection[i]);
-    } 
-  }
-  
-  // Only change code above this line
-  return arr;
+  // change code below this line
+  const length = str.length;
+  const {length : len} // change this
+  // change code above this line
+
+  return len; // you must assign length to len in line
+
 }
 
-console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
-console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 })) // should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }])
+console.log(getLength('FreeCodeCamp'));
