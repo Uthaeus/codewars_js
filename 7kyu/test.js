cf3f33402b6446db77d70e30712fce116136b70e
Uthaeus <romanlavery@gmail.com>
2018-08-07 20:17:53 -0600

    Remove leading and trailing whitespace using regex

--- 7kyu/test.js
+++ 7kyu/test.js
@@ -1,22 +1,6 @@
-function makeClass() {
-  "use strict";
-  /* Alter code below this line */
-  class Thermostat {
-    constructor(temp) {
-      this.temp = (5 / 9) * (temp - 32);
-    }
-    get temperature() {
-      return this.temp;
-    }
-    set temperature(newTemp) {
-      this.temp = newTemp;
-    }
-  }
-  /* Alter code above this line */
-  return Thermostat;
-}
-const Thermostat = makeClass();
-const thermos = new Thermostat(76); // setting in Fahrenheit scale
-let temp = thermos.temperature; // 24.44 in C
-thermos.temperature = 26;
-temp = thermos.temperature; // 26 in C
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$|\s+(?=\s)/g; // Change this line
+let result = hello.replace(wsRegex, ''); // Change this line
+
+
+console.log(result)
