f159216de86f6e915ef1d94c59da27bf94f2ef0b
Uthaeus <romanlavery@gmail.com>
2018-08-13 10:46:46 -0600

    You have a function with one side of the DNA

--- 7kyu/test.js
+++ 7kyu/test.js
@@ -1,6 +1,16 @@
-let hello = "   Hello, World!  ";
-let wsRegex = /^\s+|\s+$|\s+(?=\s)/g; // Change this line
-let result = hello.replace(wsRegex, ''); // Change this line
+function telephoneCheck(str) {
+  // Good luck!
+  let reg = /^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/;
+  
+
+  return reg.test(str);
+}
+
+console.log(telephoneCheck("555-555-5555")); // true
+console.log(telephoneCheck("(555)555-5555"));
+console.log(telephoneCheck("(555) 555-5555"));
+console.log(telephoneCheck("555 555 5555"));
+console.log(telephoneCheck("1 555 555 5555"));
 
 
-console.log(result)
+
