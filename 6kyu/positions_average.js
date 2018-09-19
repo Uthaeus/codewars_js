963af63ca51e0c5c15b8d22ef1a0a99fd4153619
Uthaeus <romanlavery@gmail.com>
2018-09-18 22:28:51 -0600

    working on positions average exercise

---
+++ 6kyu/positions_average.js
@@ -0,0 +1,34 @@
+// Given n substrings (n >= 2) in a string s our function pos_average will calculate the average percentage of positions that are the same between the (n * (n-1)) / 2 sets of substrings taken amongst the given n substrings. It can happen that some substrings are duplicate but since their ranks are not the same in s they are considered as different substrings.
+
+
+function posAverage(s) {
+    let strArr = s.replace(/\s/g, '').split(',');
+    let count = 0;
+    let total = strArr.length * strArr[0].length;
+
+    // for (let x = 0; x < strArr.length; x++) {
+    //     for (let i = 0; i < strArr[x].length; i++) {
+    //         if (count[i]) {
+    //             count[i].push(strArr[x][i]);
+    //         } else {
+    //             count[i] = [strArr[x][i]];
+    //         }
+    //     }
+    // }
+    // return count;
+    for (let x = 0; x < strArr.length - 1; x++) {
+        for (let i = 0; i < strArr[x].length; i++) {
+            if (strArr[x][i] == strArr[x + 1][i]) {
+                count++;
+            }
+            total++;
+        }
+    }
+    
+    return count / total 
+}
+
+
+
+console.log(posAverage("466960, 069060, 494940, 060069, 060090, 640009, 496464, 606900, 004000, 944096"));
+//, 26.6666666667)
