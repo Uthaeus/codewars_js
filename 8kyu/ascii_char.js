e468128eafcf7bb9a5e8796720c7207cb4a04748
Uthaeus <romanlavery@gmail.com>
2018-10-20 20:02:04 -0600

    write a function getASCII which inputs a character and returns the corresponding ascii value for that character

---
+++ 8kyu/ascii_char.js
@@ -0,0 +1,10 @@
+// write a function getASCII which inputs a character and returns the corresponding ascii value for that character. Example : getASCII('A') => 65
+
+
+function getASCII(c){
+    return c.charCodeAt(0);
+}
+
+
+
+console.log(getASCII('!')); // ,33)
