d704ac705648190463e46e874e2412dbb1888935
Uthaeus <romanlavery@gmail.com>
2018-10-21 22:27:38 -0600

    Create a function called shortcut to remove all the lowercase vowels in a given string.

---
+++ 8kyu/vowel_remover.js
@@ -0,0 +1,10 @@
+// Create a function called shortcut to remove all the lowercase vowels in a given string.
+
+
+function shortcut(string){
+    return string.replace(/[aeiou]/g, '');
+}
+
+
+
+console.log(shortcut('how are you today?')); // == 'hw r y tdy?')
