58d1423815bd167b53a238697842c9ac3f864344
Uthaeus <romanlavery@gmail.com>
2018-12-18 22:11:53 -0700

    working on email

--- 7kyu/spammy_email.js
+++ 7kyu/spammy_email.js
@@ -3,11 +3,19 @@
 
 function personalise( campaign, person){
     let result = [];
+    let arr = campaign.split(' ');
 
-    for (var x = 0; x < campaign.length; x++) {
-        let temp = campaign[x].match(/<(.*)>/)[1].replace(' ', '_').toLowerCase();
-        console.log(temp);
+    for (var x = 0; x < arr.length; x++) {
+        let temp = arr[x].match(/<(.*)>/);
+        if (temp) {
+            let re = new RegExp(temp[0], 'g');
+            let res = arr[x].replace(re, person[temp[1].replace(' ', '_').toLowerCase()]);
+            result.push(res);
+        } else {
+            result.push(arr[x]);
+        }
     }
+    return result;
 }
 
 
@@ -15,5 +23,5 @@ function personalise( campaign, person){
 let person = {name:"Sally", city:"Glasgow", favourite_products:"toasters"};
 let inps = ["Hello <NAME>","how is the weather in <CITY> today?","We wanted to let you know that <FAVOURITE PRODUCTS> are on sale today only!"];
 
-console.log(personalise(inps, person));
+console.log(personalise(inps.join(' '), person));
 // ["Hello Sally","how is the weather in Glasgow today?","We wanted to let you know that toasters are on sale today only!"];
