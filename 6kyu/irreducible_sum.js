bbe25ce5f600bcc56abca66a217a8a934fdc634d
Uthaeus <romanlavery@gmail.com>
2019-01-08 21:58:47 -0700

    in progress

--- 6kyu/irreducible_sum.js
+++ 6kyu/irreducible_sum.js
@@ -2,18 +2,30 @@
 
 
 function sumFracts(l) {
-    let ds = [], ns = [];
-    let minD = Infinity;
-
+    let ds = [], ns = [], multiplier;
+    
     for (var x = 0; x < l.length; x++) {
-        let tempD = l[x][1];
-        if (tempD < minD) {
-            minD = tempD;
-        }
-        ds.push(tempD);
+        ds.push(l[x][1]);
         ns.push(l[x][0]);
     }
-    console.log(ds, ns, minD);
+    let minD = Math.min(...ds);
+
+    for (var x = 1; ; x++) {
+        if (ds.every(a => (minD * x) % a === 0)) {
+            multiplier = x;
+            minD *= multiplier;
+            break;
+        }
+    }
+    
+    for (var x = 0; x < ns.length; x++) {
+        let temp = minD / ds[x];
+        ns[x] *= temp;
+    }
+
+    n = ns.reduce((a, b) => a += b);
+
+    return n / minD === Math.floor(n / minD) ? n / minD : [n, minD];
 }
 
 
