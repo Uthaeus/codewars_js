b51a742195fd1bbb696e232a548f36463c4b94a1
Uthaeus <romanlavery@gmail.com>
2019-01-12 22:52:57 -0700

    different

--- 7kyu/slamming_lockers.js
+++ 7kyu/slamming_lockers.js
@@ -2,23 +2,29 @@
 
 
 function lockerRun(lockers){
-    let arr = new Array(lockers).fill('c');
-    let result = [];
+    // let arr = new Array(lockers).fill('c');
+    // let result = [];
     
-    for (var x = 1; x <= lockers; x++) {
-        for (var i = x - 1; i < arr.length; i += x) {
-            if (arr[i] === 'c') {
-                arr[i] = 'o';
-            } else {
-                arr[i] = 'c';
-            }
-        }
-        console.log(arr);
-    }
-    for (var y = 0; y < arr.length; y++) {
-        if (arr[y] === 'o') {
-            result.push(y + 1);
-        }
+    // for (var x = 1; x <= lockers; x++) {
+    //     for (var i = x - 1; i < arr.length; i += x) {
+    //         if (arr[i] === 'c') {
+    //             arr[i] = 'o';
+    //         } else {
+    //             arr[i] = 'c';
+    //         }
+    //     }
+    //     console.log(arr);
+    // }
+    // for (var y = 0; y < arr.length; y++) {
+    //     if (arr[y] === 'o') {
+    //         result.push(y + 1);
+    //     }
+    // }
+    // return result;
+    let result = [];
+
+    for (var x = 1; x <= lockers; x += x) {
+        result.push(x);
     }
     return result;
 }
