2134b36f0b61ae1925a0052eae1b6a8bc09f7320
Uthaeus <romanlavery@gmail.com>
2019-12-01 22:10:43 -0700

    working on the deaf rats of hamelin exercise

--- 6kyu/deaf_rats.js
+++ 6kyu/deaf_rats.js
@@ -2,12 +2,20 @@
 
 
 var countDeafRats = function(town) {
-    
+    let arr = town.split('P'), count = 0;
+
+    for (let x = 0; x < arr.length; x++) {
+      let check = false;
+      if (x == 0) {
+        
+      }
+    }
+    return count;
 }
 
 
 
 console.log(countDeafRats("P O~ O~ ~O O~")); //, 1)
-console.log(countDeafRats("~O~O~O~O P")); //, 0))
-console.log(countDeafRats("~O~O~O~OP~O~OO~")); //, 2)
+//console.log(countDeafRats("~O~O~O~O P")); //, 0))
+//console.log(countDeafRats("~O~O~O~OP~O~OO~")); //, 2)
   
