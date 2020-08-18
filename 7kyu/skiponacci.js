2c2da557c14730cdf25934511cfb1dd535d63ec0
Uthaeus <romanlavery@gmail.com>
2020-08-18 09:55:44 -0600

    generate the Fibonacci sequence to n places

--- 7kyu/skiponacci.js
+++ 7kyu/skiponacci.js
@@ -2,8 +2,24 @@
 
 
 function skiponacci(n) {
+    let nums = [1, 1];
+    let result = [];
 
+    for (let x = 2; n >= nums.length; x++) {
+        let temp = nums[x - 1] + nums[x - 2];
+        nums.push(temp);
+    }
+    for (let x = 0; x < nums.length; x++) {
+        if (x % 2 == 0) {
+            result.push(nums[x]);
+        } else {
+            result.push('skip');
+        }
+    }
+
+    return result.slice(0, n).join(' ');
 }
 
 
 console.log(skiponacci(7)); //, "1 skip 2 skip 5 skip 13")
+console.log(skiponacci(1)); //, "1")
