e4432c67ee9822518d56a6dfa87d0db39b8bcee7
Uthaeus <romanlavery@gmail.com>
2020-09-16 20:34:32 -0600

    our method does not like the principle of carrying over numbers and just writes down every number it calculates

---
+++ 7kyu/adding.js
@@ -0,0 +1,11 @@
+// In simple terms, our method does not like the principle of carrying over numbers and just writes down every number it calculates
+
+
+function add(num1, num2) {
+    
+}
+
+
+console.log(add(2, 11)); //, 13)
+
+console.log(add(1222, 30277)); //, 31499)
