c9120e332206f6a1a942fb3dad8ce42a0cc17ab3
Uthaeus <romanlavery@gmail.com>
2020-09-18 15:27:16 -0600

    prepping roman_numbers.js

---
+++ 6kyu/roman_numbers.js
@@ -0,0 +1,9 @@
+// Your goal is to determine how many distinct integers could be represented by exactly n Roman digits grouped together
+
+
+function solve(n){
+    //
+}
+
+
+console.log(solve(4)); //,35)
