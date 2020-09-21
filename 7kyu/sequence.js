64817d1efaa4b22e9ac169f3cc6d687228266075
Uthaeus <romanlavery@gmail.com>
2020-09-21 15:08:31 -0600

    completed sequence.js

--- 7kyu/sequence.js
+++ 7kyu/sequence.js
@@ -2,7 +2,23 @@
 
 
 function sumOfN(n) {
-    // insert here your code
+    let result = [];
+    let len = n < 0 ? (n * -1) + 1 : n + 1;
+    
+    for (let x = 0; x < len; x++) {
+        let temp = [];
+        let y = 0;
+        while (y <= x) {
+            temp.push(y);
+            y++;
+        }
+        let tempX = temp.reduce((a, b) => a += b);
+        if (n < 0 && tempX != 0) {
+            tempX = tempX * -1;
+        }
+        result.push(tempX);
+    }
+    return result;
 };
 
 
