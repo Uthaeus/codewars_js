785dcb883c133292a3773fd508532d4eb6ee3fa4
Uthaeus <romanlavery@gmail.com>
2020-09-21 14:46:44 -0600

    Complete the function that takes an integer n and returns a list/array of length abs(n) + 1 of the arithmetic series

---
+++ 7kyu/sequence.js
@@ -0,0 +1,9 @@
+// Complete the function that takes an integer n and returns a list/array of length abs(n) + 1 of the arithmetic series
+
+
+function sumOfN(n) {
+    // insert here your code
+};
+
+
+console.log(sumOfN(-4)); //, [0, -1, -3, -6, -10])
