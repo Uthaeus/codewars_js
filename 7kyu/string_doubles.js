2964f2493933143be7f9ae91949674e1da7a7225
Uthaeus <romanlavery@gmail.com>
2020-10-02 20:26:30 -0600

    working with do/while loop

--- 7kyu/string_doubles.js
+++ 7kyu/string_doubles.js
@@ -2,24 +2,30 @@
 
 
 function doubles(s){
-    let tempStr = '';
-    let result = '';
+    let tempStr = '', result = '';
 
     for (let x = 0; x < s.length; ) {
         let temp = s[x];
         do {
             tempStr += s[x];
-            x++
-        }while (s[x] == temp);
-        
+            x++;
+        } while (s[x] == temp);
+
         if (tempStr.length % 2 != 0) {
             result += temp;
         }
         tempStr = '';
     }
+    console.log(result);
     return result;
 }
 
 
 console.log(doubles('zzzzykkkd')); //,'ykd')
 console.log(doubles('vvvvvoiiiiin')); //,'voin')
+
+let tester = "zqqbcclnkktssvbeefccggoozyylwwziiookkxjvnzcclccggdpssqqvxuukkwwryyztsstpsswwdlssyyuuiihtpjzeessoofmmiilfvccdpdmmssyykkwwtcciidkkyyheehmmeeaammlnwwjeenhnnyyhlhlzcclxccplmmoofwwvzptnoojrdaammyyvuuaaggntlmmmmyymmggxfvtssssmmpwwhccuuggggldvfyyyyxuuoohxaabccmmooroooodoojwwwwvxoofyyssdyybpdaazjqqjpaafssiiwwzkknyyyyfbggggteedsszmmrkkbooheejjvtjiiuubccqqiigglyyooyyoobxvqqaanaaqqprlprlssbvpiivuufbnccxjggnmmuuggxblggssccdssyyaauuzssmmuurhyyyyoofsswwmmvgglgghwwhbdaayytssvjsssszggccmmgglyybjqqeerqqtccccpdjxbhiinryyaabnzyyyyggrkkvxjzqqyytleesseepmmvpaaxtkkxwwqqbqqxmmdmmjaapxfsswwluucceejxaabtssooeeaajuuiiziiyyhheediissjiipxhggpdliijjjrzaaggoojnhyyuudjxsshyyeedxnuuyyyyhyyqqlzggxxhbggmmccuutggmmjbwwwwyynkklxdbwwxrccfluudvkksseeuuyyoonqquuaavzccpxyymmfnqqnssvvxbuummhjmmjuussvcchpggpxqqfiitxccyyrzpvyyqqlqqkkkkeevssvaavdoouueedzpfoonoosskkvrvxssfggxeedaahvooyykkxqqaauukkuurrvwwdtzrduummaadwwyyzssoosstrwwdpmmmmmmeewwvfeeaabaakklwwkkrttaafsspuuooccuuziiggtlfxuueesseekkbqqltrhggnlpssqqxfftoommyyssfeeyyeeuujggjveekkccccdyybaayyssjaapdboofdrhggssuudaaraaloohrtpzssoooosswwuujjuuuussvssxeeaaggeeeepvbhuuggtggwwtwwmmssyyzeenoojiiaarxccjkkfoocctmmfccziiuukkrlrtlfkkkkfeeuutoowwbssmmsswweeoossmmkkeedtjzeeprkktxrggjrooaahaatdqqcclaafhnpkkkkuubeevwwssmmjtwwccggtlzmmggggzoosshjuuoossxooeeiiaanwwtprbpeewwddboommwwaammqqsscchmmeejfncctggggzoowwhqqxuuyyrmmpmmrxwweeqqpooggeehkkhiikkpxoosseetryygguuiipbaaccnyyuuqqiioormmvqqvjjzxvmmuudxjyybftooccccjlmmuuzyyqqjtpqqeepkkccrphiiiinssfooxyyvssgguurmmkkoorjggpnxljggvssrtjaaccbbiildtdpveeccqqztiipnqqlncchjmmuussggbmmooddrfpvpfxooaaaaeeaauuyyeepfwwmmqqloovjuuthaaleewwphbpmmeeffjggtaabuujdxvtmmkkmmxyyyyrqqzmmlvyyjmmzwwnjsspggzblmmbssvssxnssmmyyaavyyoossryypdmmmmuurdqqzrkkbjyyfaauuhtxxreemmccrwwqqljdmmfccmmeeoooojptmmrggwwcceeaajmmfaaiitmmjkkssccmmrvxccqquueepuueertrssxkkziipvn";
+
+let myAns = doubles(tester);
+let ans = 'zblntvbfzlzxjvnzldpvxrzpdlhtpjzflfvdpdtdlnjnlhlzlxplfvzptnjrdvntlxfvtphldvfxhxbrdjvxfdbpdzpfznfbtdzrbhvtjblbxvnprlprlbvpvfbnxjnxbldzrhfvlbdtvjzlbjrtpdjxbhnrbnzrvxjztlpvpxtxbxdjpxfljxbtjzdjpxhpdljrzjnhdjxhdxnhlzhbtjbnlxdbxrfldvnvzpxfxbhvhxftxrzpvlvzpfnvrvxfxdhvxvdtzrztrdpvfblrfpztlfxbltrhnlpxtfvdbjpdbfdrhdrlhrtpzvxpvbhznjrxjftfzrlrtltbdtjzprtxrjrhtdlfhnpbvjlhjxntprbpbhjfntzhxrprtrpbnrzxvdxjbftjlzjtrphnfxvjpnxljvrtjldtdpvztpnlnhjbrfpvpfxpflvjthlphbpjtbjdxvtxrzlvjznjpzblbvxnvrpdrdzrbjfhtljdfjptrjftjrvxprtrxzpvn';
+console.log(myAns == ans);
