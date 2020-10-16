c9f0a5cc7d5c99e157dc7b4bac15463936cd3f8f
Uthaeus <romanlavery@gmail.com>
2020-10-16 15:24:03 -0600

    submitted refactored function

--- 7kyu/remove_bmw.js
+++ 7kyu/remove_bmw.js
@@ -1,22 +1,31 @@
 // Your task is to write a function that takes one parameter str that MUST be a string and removes all capital and small letters B, M and W. If data of the wrong data type was sent as a parameter the function must throw an error (IllegalArgumentException in Groovy): "This program only works for text."
 
 
+// function removeBMW(str){
+//     if (allLetters(str)) {
+//         str = str.replace(/[bmw]/ig, '');
+//     } else {
+//         return "This program only works for text."
+//     }
+//     return str;
+// }
+
+// function allLetters(s) {
+//     let letters = /^[A-Za-z]+$/;
+//     if (s.match(letters)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
 function removeBMW(str){
-    if (allLetters(str)) {
-        str = str.replace(/[bmw]/ig, '');
+    if (typeof str !== 'string') {
+        throw Error('This program only works for text.');
     } else {
-        return "This program only works for text."
+        str = str.replace(/[bmw]/ig, '');
     }
     return str;
-}
-
-function allLetters(s) {
-    let letters = /^[A-Za-z]+$/;
-    if (s.match(letters)) {
-        return true;
-    } else {
-        return false;
-    }
 }
 
 
