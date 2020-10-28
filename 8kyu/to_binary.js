f7566215dca3b2d101aac343921b19130875f423
Uthaeus <romanlavery@gmail.com>
2020-10-28 12:51:09 -0600

    Given a non-negative integer n, write a function toBinary/ToBinary which returns that number in a binary format

---
+++ 8kyu/to_binary.js
@@ -0,0 +1,9 @@
+// Given a non-negative integer n, write a function toBinary/ToBinary which returns that number in a binary format.
+
+
+function toBinary(n){
+    return +n.toString(2);
+}
+
+
+console.log(toBinary(2)); //, 10
